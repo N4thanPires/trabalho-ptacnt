ba83ce2db20d04c16c8a12b4034fb1692e48bd10
Nathan Pires <129331480+N4thanPires@users.noreply.github.com>
2023-12-12 11:31:05 -0300

    arumando page do dashboard e colocando css

--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -1,24 +1,34 @@
+import Navbar from '@/app/componentes/navbar';
+import { getUsers } from '@/app/functions/handlerAcessAPI';
+import Link from 'next/link';
+
+import styles from './dash.css'; // Ajuste o caminho conforme necessário
+
 export default async function Dashboard() {
     try {
         const users = await getUsers();
-        
+
         return (
             <div>
                 <Navbar />
-                {users?.map((user, index) =>
-                    <div key={index}>
-                        <h1>{user.name} {user.password}</h1>
-                    </div>
-                )}
+                <div className={styles.userContainer}>
+                    {users?.map((user, index) => (
+                        <div key={index} className={styles.userCard}>
+                            <Link href={`/pages/alterar/${user.id}`}>
+                                <h1>{user.name} {user.email} {user.password}</h1>
+                            </Link>
+                        </div>
+                    ))}
+                </div>
             </div>
         );
     } catch (error) {
         console.error("Erro ao obter usuários:", error);
-        // Pode adicionar uma mensagem de erro na interface do usuário se necessário
+
         return (
             <div>
                 <Navbar />
-                <p>Erro ao obter usuários. Tente novamente mais tarde.</p>
+                <p>Erro ao obter usuários.</p>
             </div>
         );
     }
