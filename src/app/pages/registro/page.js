15a79bb27a6eee42082a6a5cd1bcc2fe915e49af
Nathan Pires <129331480+N4thanPires@users.noreply.github.com>
2023-12-12 11:30:43 -0300

    arrumando page de registro

--- src/app/pages/registro/page.js
+++ src/app/pages/registro/page.js
@@ -1,25 +1,39 @@
 'use client' 
 
-import React, { useState } from 'react'; // Importa o módulo React e a função useState.
-import { ToastContainer, toast } from 'react-toastify'; // Importa os componentes ToastContainer e toast do módulo react-toastify.
-import 'react-toastify/dist/ReactToastify.css'; // Importa os estilos CSS para o react-toastify.
-import { useRouter } from 'next/router'; // Importa a função useRouter do módulo next/router.
-import Navbar from '@/app/componentes/navbar'; // Importa o componente Navbar de um caminho específico no projeto.
-import '../css/registro.css'; // Importa o arquivo CSS de estilo para esta página.
+import React, { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify'; 
+import 'react-toastify/dist/ReactToastify.css'; 
+import { useRouter } from 'next/navigation'; 
+import Navbar from '@/app/componentes/navbar'; 
+import '../css/registro.css'; 
+import { postUser } from '@/app/functions/handlerAcessAPI';
+
 
 export default function Registro() {
+
   const [user, setUser] = useState({
     name: '',
     email: '',
-    password: '',
+    password: ''
   });
 
-  const { push, refresh } = useRouter();
+  const { push } = useRouter();
 
-  function registro(e) {
+  const registro = async (e) => {
     e.preventDefault();
-    toast.success('feito');
-  }
+    try{
+      await postUser(user);
+      await new Promise((resolve) => {
+        toast.success("Registrado com suxso");
+        setTimeout(resolve, 5000);
+      });
+      return push("/pages/dashboard") 
+    }
+    catch{
+       return toast.error("erro")
+    }
+  };
+  
 
   return (
     <div>
@@ -34,7 +48,7 @@ export default function Registro() {
           <input type="password" name="password" required value={user.password} onChange={(e) => setUser({ ...user, password: e.target.value })} />
           <p></p>
           <button className="btn" type="submit">
-            Cadastrar
+            Registrar
           </button>
         </div>
       </form>
