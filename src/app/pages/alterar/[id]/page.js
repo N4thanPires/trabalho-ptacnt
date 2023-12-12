f69b39eb2fbe7aa5bf385e184c0d70f6ef282693
Nathan Pires <129331480+N4thanPires@users.noreply.github.com>
2023-12-12 16:10:51 -0300

    corrijindo erro

--- src/app/pages/alterar/[id]/page.js
+++ src/app/pages/alterar/[id]/page.js
@@ -5,6 +5,8 @@ import 'react-toastify/dist/ReactToastify.css';
 import Navbar from "@/app/componentes/navbar";
 import { updateUser } from "@/app/functions/handlerAcessAPI";
 import {useRouter} from 'next/navigation';
+import './alterar.css'
+
 
 
 export default function Alterar({params}) {
@@ -17,35 +19,52 @@ password:''
 
   const { push } = useRouter();
 
- const alterar= async (e) => {
+  const alterar = async (e) => {
     e.preventDefault();
-    try{
-    await updateUser(user, params.id);
-    await new Promise((resolve) => {
-      toast.success("boaa altero");
-      setTimeout(resolve, 5000);
-    });
-    return push("/pages/dashboard") 
+    try {
+      await updateUser(user, params.id);
+      await new Promise((resolve) => {
+        toast.success("Alteração realizada com sucesso!");
+        setTimeout(resolve, 5000);
+      });
+      return push("/pages/dashboard")
+    } catch {
+      return toast.error("Erro ao realizar a alteração");
+    }
   }
-  catch{
-     return toast.error("erro")
-  }
-}
 
   return (
-    <div>
+    <div className="alterar-container">
       <Navbar />
-      <form onSubmit={alterar}>
-        <div className="caixa">
-          <p>NOME</p>
-          <input type="text" required  onChange={(e) => { setUser({ ...user, name: e.target.value })}} />
-          <p>EMAIL</p>
-          <input type="email" required  onChange={(e) => { setUser({ ...user, email: e.target.value })}} />
-          <p>SENHA</p>
-          <input type="password" required  onChange={(e) => { setUser({ ...user, password: e.target.value })}} />
-          <p></p>
-          <button className="btn">Alterar</button>
+      <form onSubmit={alterar} className="form-container">
+        <div className="form-group">
+          <label htmlFor="name">NOME</label>
+          <input
+            type="text"
+            id="name"
+            required
+            onChange={(e) => { setUser({ ...user, name: e.target.value }) }}
+          />
         </div>
+        <div className="form-group">
+          <label htmlFor="email">EMAIL</label>
+          <input
+            type="email"
+            id="email"
+            required
+            onChange={(e) => { setUser({ ...user, email: e.target.value }) }}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">SENHA</label>
+          <input
+            type="password"
+            id="password"
+            required
+            onChange={(e) => { setUser({ ...user, password: e.target.value }) }}
+          />
+        </div>
+        <button className="btn">Alterar</button>
       </form>
       <ToastContainer />
     </div>
